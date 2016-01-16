0b7f90808dfeaafb569970bc755fc25b30693219
Potato42 <bburrill98@gmail.com>
2016-01-15 21:04:18 -0800

    Created example program Hopefully, this will make it easier to understand how to play around with the back end.

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
 import kacpaw
+
+# construct a User object representing T#1B12P
+tn1b12p = kacpaw.User.from_username("tn1b12p")
+
+print(tn1b12p.name)
+
+# this is the POTATO! program
+potato_program = kacpaw.Program("5495235907551232")
+
+print(potato_program.title, "at", potato_program.url)
+
+# print out all the comments (please no encoding issues, please...)
+for comment in potato_program.get_replies():
+    print(comment.text_content)
